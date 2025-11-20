3c4fd059464bcc53738c98548268cea8951f61d4
Pavlo <86185982+lyahpavel@users.noreply.github.com>
2025-11-20 23:00:21 +0200

    Fix: додано important для submission-display + v2.0.2

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Змінюй цю версію при кожному оновленні додатку!
-const CACHE_VERSION = 'v2.0.1';
+const CACHE_VERSION = 'v2.0.2';
 const CACHE_NAME = `drone-reports-${CACHE_VERSION}`;
 const urlsToCache = [
   './',
