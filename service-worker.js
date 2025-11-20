ec4b4303fe1c86b0794563b6ef9045f63bec4c73
Pavlo <86185982+lyahpavel@users.noreply.github.com>
2025-11-20 23:30:44 +0200

    Fix: зменшено селекти кількості вдвічі v2.0.9

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Змінюй цю версію при кожному оновленні додатку!
-const CACHE_VERSION = 'v2.0.8';
+const CACHE_VERSION = 'v2.0.9';
 const CACHE_NAME = `drone-reports-${CACHE_VERSION}`;
 const urlsToCache = [
   './',
